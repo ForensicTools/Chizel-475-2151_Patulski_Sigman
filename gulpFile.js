8d59673835ec5e9ba63dbe44d2e72ab8cc737d32
patulskitime <patulskitime@gmail.com>
2015-11-18 00:24:17 -0500

    tryed adding livereload

--- gulpFile.js
+++ gulpFile.js
@@ -3,17 +3,16 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     less = require('gulp-less'),
     concat = require('gulp-concat');
-
+//     electron = require('electron-connect').server.create( //{
+// //   electron: require('electron-prebuilt')
+// // // }
+// );
+//
 
 var coffeeFile = '*.coffee';
-
 var coffeeSource = 'src/';
 var coffeeDest = 'static/scripts/';
-
-var coffeeDir =
-    ['browser/',
-    'backside/',
-    'render/'];
+var coffeeDir = ['browser/','backside/','render/'];
 
 var jquerySource = ['node_modules/jquery/dist/*'];
 
@@ -26,11 +25,24 @@ var bootstrapDest = ['static/lib/bootstrap/css/',
                      'static/lib/bootstrap/js/'];
 
 var treeviewSource = ['node_modules/bootstrap-treeview/dist/*'];
-
 var treeviewDest = 'static/lib/bootstrap-treeview/';
 
 var lessSource = 'static/less/*.less';
 var lessDest = 'static/css/';
+
+
+// gulp.task('serve', function(){
+//     console.log(process.cwd())
+//     electron.start();
+//     gulp.watch(['static/scripts/backside/*','static/scripts/browser/*'],['electron:restart']);
+//     gulp.watch(['static/index.html','static/lib/*','static/scripts/render/*'],['electron:reload']);
+// });
+// gulp.task('electron:restart', function(){
+//     electron.restart();
+// });
+// gulp.task('electron:reload', function(){
+//     electron.reload();
+// });
 
 gulp.task('coffee', function() {
     for (var _i = 0; _i < coffeeDir.length; _i++)
@@ -50,7 +62,6 @@ gulp.task('less', function(){
     .pipe(gulp.dest(lessDest));
 });
 
-
 gulp.task('jquery', function (){
   gulp.src(jquerySource).pipe(gulp.dest('static/lib/jquery/'));
 });
@@ -67,11 +78,13 @@ gulp.task('bootstrapFont', function(){
 gulp.task('bootstrap-treeview', function(){
     gulp.src(treeviewSource).pipe(gulp.dest(treeviewDest));
 });
+
 gulp.task('genlib',['jquery', 'bootstrap', 'bootstrapFont','bootstrap-treeview']);
 
 gulp.task('watch', function(){
     gulp.watch('src/**/*.coffee', ['coffee']);
     gulp.watch('static/less/*.less', ['less']);
+
 });
 
 
